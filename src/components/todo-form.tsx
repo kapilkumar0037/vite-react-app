2eac486c29e5f18891c01b1fd189fbb48b45be60
Kapil Kumar <kapilkumar0037@gmail.com>
2025-05-17 19:27:51 +0530

    ui updated

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -3,13 +3,18 @@ import { useState } from "react";
 export default function TodoForm({ onCreateTodo }: any) {
   const [todoText, setTodoText] = useState("");
   return (
-    <div>
-      <input className="form-control"
+    <div
+      style={{ display: "flex", gap: "10px", justifyContent: "space-around" }}
+    >
+      <input
+        className="form-control"
         type="text"
         placeholder="Add a new todo"
         onChange={(e) => setTodoText(e.target.value)}
       />
-      <button className="btn btn-primary"
+      <button
+        className="btn btn-primary"
+        style={{ whiteSpace: "nowrap" }}
         onClick={() => {
           onCreateTodo({ id: 5, title: todoText, completed: false });
           setTodoText("");
--- src/components/todo-list-item.tsx
+++ src/components/todo-list-item.tsx
@@ -7,9 +7,9 @@ export default function TodoListItem({
     <tr>
       <td>{todo.id}</td>
       <td>{todo.title}</td>
-      <td>{todo.completed}</td>
+      <td>{todo.completed.toString()}</td>
       <td>
-      {todo.completed ? <button onClick={()=>onDeleteClicked(todo.id)}>Delete</button> : <button onClick={()=>onCompletedClicked(todo.id)}>Complete</button>}
+      {todo.completed ? <button className="btn btn-danger" onClick={()=>onDeleteClicked(todo.id)}>Delete</button> : <button className="btn btn-success" onClick={()=>onCompletedClicked(todo.id)}>Complete</button>}
 
       </td>
 
